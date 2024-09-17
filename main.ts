313a10219afab624e4959c33fc7f49dac501e951
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-09-17 11:02:50 -0400

    added calulations of perimter and area

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Pablo
+ * Created on: Sep 2024
+ * This program calculates area and perimeter 
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+//perimeter
+basic.showString('5 * 2 + 3 * 2 =' + (5 * 2 + 3 * 2).toString())
+basic.pause(100)
+basic.clearScreen()
+
+//area
+basic.showString('5 * 3 =' + (5 * 3).toString())
+basic.pause(100)
+basic.clearScreen()
